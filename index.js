a041c39bf76d90c7f2dc0213cd8ad08202bbe82e
rhapsodhy <me@rhapsodhy.hu>
2014-10-16 03:01:52 +0100

    [fix] update to fail properly with jest on my system

--- index.js
+++ index.js
@@ -4,19 +4,17 @@ var jest = require('jest-cli'),
     gutil = require('gulp-util'),
     through = require('through2');
 
-module.exports = function (options) {    
+module.exports = function (options) {
     options = options || {};
     return through.obj(function (file, enc, cb) {
         options.rootDir = options.rootDir || file.path;
-        try {
-            jest.runCLI({
-                config: options
-            }, options.rootDir, function () {
-                cb();
-            }.bind(this));
-        } catch (err) {
-            this.emit('error', new gutil.PluginError('gulp-jest', err));
+        jest.runCLI({
+            config: options
+        }, options.rootDir, function (success) {
+            if(!success) {
+                cb(new gutil.PluginError('gulp-jest', { message: "Tests Failed" }));
+            }
             cb();
-        }
+        }.bind(this));
     });
 };
