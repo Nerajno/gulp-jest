2d5da6bdc6e9c7a16e1d5ac531d6fea81db75fe2
cookfront <cookfront@gmail.com>
2014-12-23 20:54:55 +0800

    fix `options.rootDir` problem

--- test.js
+++ test.js
@@ -6,39 +6,18 @@ var jest = require('./index');
 var through2 = require('through2');
 var out = process.stdout.write.bind(process.stdout);
 
-it('should take a rootDir as an option', function (cb) {
-    var stream = jest({
-        rootDir: '__tests__'
+describe('gulp-jest', function () {
+    it('should use the scream path as the rootDir', function (cb) {
+        var stream = gulp.src('__tests__')
+            .pipe(jest());
+
+        process.stdout.write = function (str) {
+            out(str);
+            if (/test passed/.test(str)) {
+                assert(true);
+                process.stdout.write = out;
+                cb();
+            }
+        };
     });
-
-    process.stdout.write = function (str) {
-        out(str);
-        if (/test passed/.test(str)) {
-            assert(true);
-            process.stdout.write = out;
-            cb();
-        }
-    };
-
-    stream.write(new gutil.File({
-        path: '__tests__',
-        contents: new Buffer('')
-    }));
-
-    stream.end();
-});
-
-
-it('should use the scream path as the rootDir', function (cb) {
-    var stream = gulp.src('__tests__')
-        .pipe(jest());
-
-    process.stdout.write = function (str) {
-        out(str);
-        if (/test passed/.test(str)) {
-            assert(true);
-            process.stdout.write = out;
-            cb();
-        }
-    };
 });
