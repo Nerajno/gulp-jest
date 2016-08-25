411b1dd106502ddb53aaac970d5c11ea13a2ae59
Alan Souza <alansouzati@gmail.com>
2016-08-25 14:38:19 -0700

    Added watch support

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ module.exports = function (options) {
     options = options || {};
     return through.obj(function (file, enc, cb) {
         options.rootDir = options.rootDir || file.path;
+        var watch = options.watch ? options.watch : undefined;
+        delete options.watch;
         jest.runCLI({
             config: options
         }, options.rootDir, function (success) {
-            if(!success) {
+            if(!success && !watch) {
                 cb(new gutil.PluginError('gulp-jest', { message: "Tests Failed" }));
             } else {
                 cb();
