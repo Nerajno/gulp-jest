5d559f15b4a3633ccdee55ae21cbd741f6d0f472
Alan Palazzolo <alan.palazzolo@startribune.com>
2017-07-10 11:16:02 -0500

    Fixing the use of vinyl sources.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import jest from 'jest-cli';
 import gutil from 'gulp-util';
 import through2 from 'through2';
+import defaultsDeep from 'lodash.defaultsdeep';
 
-export default (options = {}) => {
+export default (defaultOptions = {}) => {
   return through2.obj((file, enc, cb) => {
-    options = Object.assign({}, options, {
-      config: Object.assign({
-        rootDir: file ? file.path : undefined
-      }, options.config)
-    });
+    // Allow for '__tests__' to just use current cwd.
+    let options = defaultsDeep({}, { config: {
+      rootDir: file && !file.path.match(/.*__tests__/i) ? file.path : undefined
+    }}, defaultOptions);
 
     jest.runCLI(options, [options.config.rootDir], (result) => {
       if(result.numFailedTests || result.numFailedTestSuites) {
