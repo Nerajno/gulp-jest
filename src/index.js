74a7cae8bb152273e68fe0806f8d78a65b2c1c0d
Alan Souza <alansouzati@gmail.com>
2017-11-02 13:53:12 -0700

    Fixed tests

--- src/index.js
+++ src/index.js
@@ -4,18 +4,16 @@ import through2 from 'through2';
 
 export default (options = {}) => {
   return through2.obj((file, enc, cb) => {
-    options = Object.assign({}, options, {
-      config: Object.assign({
-        rootDir: file ? file.path : undefined
-      }, options.config)
-    });
+    options = Object.assign({
+      rootDir: file ? file.path : undefined
+    }, options);
 
-    jest.runCLI(options, [options.config.rootDir], (result) => {
-      if(result.numFailedTests || result.numFailedTestSuites) {
+    jest.runCLI(options, [options.rootDir]).then(({ results }) => {
+      if(results.numFailedTests || results.numFailedTestSuites) {
         cb(new gutil.PluginError('gulp-jest', { message: 'Tests Failed' }));
       } else {
         cb();
       }
     });
-  })
+  });
 };
